61c676466360cdd0800006c4ef93ac7f43645090
Tobias Lediger <tobias.lediger@student.fh-wuerzburg.de>
2015-04-22 15:13:18 +0200

    erste Schritte Cutscene

---
+++ JosieGame/Classes/Cutscene.h
@@ -0,0 +1,38 @@
+/*
+ * Cutscene.h
+ *
+ *  Created on: 21.04.2015
+ *      Author: ryban
+ */
+
+#ifndef CUTSCENE_H_
+#define CUTSCENE_H_
+
+#include "cocos2d.h"
+
+
+
+class Cutscene : public cocos2d::Layer {
+public:
+	// there's no 'id' in cpp, so we recommend returning the class instance pointer
+	    static cocos2d::Scene* createScene();
+
+	    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+	    virtual bool init();
+
+	    // a selector callback
+	    void menuCloseCallback(cocos2d::Ref* pSender);
+
+	    // implement the "static create()" method manually
+	    CREATE_FUNC(Cutscene);
+
+		void play(cocos2d::Ref* pSender);
+		void pause(cocos2d::Ref* pSender);
+
+
+
+	Cutscene();
+	virtual ~Cutscene();
+};
+
+#endif /* CUTSCENE_H_ */
