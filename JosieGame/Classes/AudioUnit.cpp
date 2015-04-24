30e052303c0b7a6e8a8f29c5fef7e9c0992d91e7
Daniel Mügge <hello@daniel-muegge.de>
2015-04-24 23:23:58 +0200

    AudioUnit Class added

---
+++ JosieGame/Classes/AudioUnit.cpp
@@ -0,0 +1,22 @@
+/*
+ * AudioUnit.cpp
+ *
+ *  Created on: 24.04.2015
+ *      Author: danielmugge
+ */
+
+#include "AudioUnit.h"
+#include "SimpleAudioEngine.h"
+
+namespace cocos2d {
+
+AudioUnit::AudioUnit() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AudioUnit::~AudioUnit() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace cocos2d */
