7422aa47763fd3a8ce0b933b2232143ec75ead82
Jonas Kaiser <hello@kaiserjonas.de>
2015-04-24 23:48:09 +0200

    added SpriteUnit Class

---
+++ JosieGame/Classes/SpriteUnit.cpp
@@ -0,0 +1,18 @@
+/*
+ * SpriteUnit.cpp
+ *
+ *  Created on: 24.04.2015
+ *      Author: Jonas
+ */
+
+#include "SpriteUnit.h"
+
+SpriteUnit::SpriteUnit() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SpriteUnit::~SpriteUnit() {
+	// TODO Auto-generated destructor stub
+}
+
