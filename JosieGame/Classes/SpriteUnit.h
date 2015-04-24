895ca6509e172a95d46f19fda7a94dc487142970
Jonas Kaiser <hello@kaiserjonas.de>
2015-04-24 23:48:09 +0200

    added SpriteUnit Class

---
+++ JosieGame/Classes/SpriteUnit.h
@@ -0,0 +1,17 @@
+/*
+ * SpriteUnit.h
+ *
+ *  Created on: 24.04.2015
+ *      Author: Jonas
+ */
+
+#ifndef SPRITEUNIT_H_
+#define SPRITEUNIT_H_
+
+class SpriteUnit {
+public:
+	SpriteUnit();
+	virtual ~SpriteUnit();
+};
+
+#endif /* SPRITEUNIT_H_ */
