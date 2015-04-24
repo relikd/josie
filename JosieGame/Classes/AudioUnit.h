3e83bce97de500b7cd48f92915b91887e8a5fad9
Daniel Mügge <hello@daniel-muegge.de>
2015-04-24 23:23:58 +0200

    AudioUnit Class added

---
+++ JosieGame/Classes/AudioUnit.h
@@ -0,0 +1,22 @@
+/*
+ * AudioUnit.h
+ *
+ *  Created on: 24.04.2015
+ *      Author: danielmugge
+ */
+
+#ifndef AUDIOUNIT_H_
+#define AUDIOUNIT_H_
+
+
+namespace cocos2d {
+
+class AudioUnit {
+public:
+	AudioUnit();
+	virtual ~AudioUnit();
+};
+
+} /* namespace cocos2d */
+
+#endif /* AUDIOUNIT_H_ */
