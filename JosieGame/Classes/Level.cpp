4c0df99f2f1ce85da4dcb8eaa5c734c441a33560
Jonas Kaiser <hello@kaiserjonas.de>
2015-04-25 14:59:36 +0200

    änderung an collectAt() um Fehler bei fehlenden Foreground_layer zu verhindern

--- JosieGame/Classes/Level.cpp
+++ JosieGame/Classes/Level.cpp
@@ -13,24 +13,21 @@ int _res_index_sub;
 Size visibleSize;
 Vec2 origin;
 
-
-Level::Level()
-{
+Level::Level() {
 	visibleSize = Director::getInstance()->getVisibleSize();
 	origin = Director::getInstance()->getVisibleOrigin();
-	player=NULL;
-	playerControl=NULL;
-	audioUnit=NULL;
+	player = NULL;
+	playerControl = NULL;
+	audioUnit = NULL;
 
 	_tileMap = NULL;
 	_tilemapBackground = NULL;
 	_metaLayer = NULL;
 }
-Level::~Level(){}
+Level::~Level() {
+}
 
-
-Scene* Level::createScene(int level, int sublevel)
-{
+Scene* Level::createScene(int level, int sublevel) {
 
 	_res_index = level;
 	_res_index_sub = sublevel;
@@ -49,37 +46,32 @@ Scene* Level::createScene(int level, int sublevel)
 }
 
 // on "init" you need to initialize your instance
-bool Level::init()
-{
-	if (!Layer::init())
-	{
+bool Level::init() {
+	if (!Layer::init()) {
 		return false;
 	}
 
 	addBackground();
-	
+
 	addTilemap();
 
 	addPauseButton();
 	addAudio();
-    addPlayer();
-    addPlayerControl();
+	addPlayer();
+	addPlayerControl();
 	return true;
 
 }
 
-
 //Method Called by Pausebutton -> "goes back" to MainMenu
-void Level::pause(Ref* pSender)
-{
+void Level::pause(Ref* pSender) {
 	audioUnit->stopBackground();
 	delete audioUnit;
 	Director::getInstance()->popScene();
 }
 
-void Level::addTilemap()
-{//Add TileMap
-	//lade Tilemap aus Resource Ordner in Abhaenggkeit der uebergebenen level-Nummer
+void Level::addTilemap() { //Add TileMap
+//lade Tilemap aus Resource Ordner in Abhaenggkeit der uebergebenen level-Nummer
 	std::ostringstream tilemap;
 	tilemap << "tilemaps/" << _res_index << "." << _res_index_sub << ".tmx";
 	_tileMap = TMXTiledMap::create(tilemap.str());
@@ -92,26 +84,25 @@ void Level::addTilemap()
 	}
 }
 
-void Level::addBackground()
-{
+void Level::addBackground() {
 	//index to string for background loading
 	std::ostringstream s;
-	s << "backgrounds/bg_"<< _res_index <<"."<<_res_index_sub << ".png";
+	s << "backgrounds/bg_" << _res_index << "." << _res_index_sub << ".png";
 
 	//Add Background Image
-		auto background = Sprite::create(s.str());
-		background->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
-		this->addChild(background, 0);
+	auto background = Sprite::create(s.str());
+	background->setPosition(
+			Vec2(visibleSize.width / 2 + origin.x,
+					visibleSize.height / 2 + origin.y));
+	this->addChild(background, 0);
 }
 
-void Level::addAudio()
-{
-	audioUnit = AudioUnit::initWithLevel(1,1);
+void Level::addAudio() {
+	audioUnit = AudioUnit::initWithLevel(1, 1);
 	audioUnit->playBackground();
 }
 
-void Level::addPlayer()
-{
+void Level::addPlayer() {
 	//Add Player
 	player = Player::initWithLevel(this);
 	player->setPosition(Vec2(origin.x + visibleSize.width / 5, origin.y + 288));
@@ -120,24 +111,22 @@ void Level::addPlayer()
 
 }
 
-void Level::addPlayerControl()
-{
+void Level::addPlayerControl() {
 	//add control to scene
-    playerControl = PlayerControl::initWithLevel(this);
-    this->addChild(playerControl,-1);
+	playerControl = PlayerControl::initWithLevel(this);
+	this->addChild(playerControl, -1);
 }
 
-void Level::addPauseButton()
-{
+void Level::addPauseButton() {
 	//Add Pause Button in upper right corner
 	auto pause = MenuItemImage::create("buttons/pausebutton.png",
-										"buttons/pausebutton.png",
-										CC_CALLBACK_1(Level::pause,this));
-	pause->setPosition(origin.x + visibleSize.width - pause->getContentSize().width,
-						origin.y + visibleSize.height -pause->getContentSize().height);
+			"buttons/pausebutton.png", CC_CALLBACK_1(Level::pause, this));
+	pause->setPosition(
+			origin.x + visibleSize.width - pause->getContentSize().width,
+			origin.y + visibleSize.height - pause->getContentSize().height);
 	auto menu = Menu::create(pause, NULL);
-			    menu->setPosition(Vec2::ZERO);
-			    this->addChild(menu, 1);
+	menu->setPosition(Vec2::ZERO);
+	this->addChild(menu, 1);
 
 }
 
@@ -149,8 +138,7 @@ Point Level::getTileAt(Point position) {
 	return Point(x, y);
 }
 
-TilePropertyType Level::getTileProperty(Point position)
-{
+TilePropertyType Level::getTileProperty(Point position) {
 	Point tileCoord = getTileAt(position);
 	int tileGID = _metaLayer->getTileGIDAt(tileCoord);
 	Value propMap = _tileMap->getPropertiesForGID(tileGID);
@@ -166,11 +154,11 @@ TilePropertyType Level::getTileProperty(Point position)
 }
 
 //entfernt an �bergebener Position das Tile aus dem Foreground_layer und das dazugeh�rige Tile aus dem Meta_layer
-void Level::collectAt(Point position)
-{
+void Level::collectAt(Point position) {
 	Point TileCoord = getTileAt(position);
-	TMXLayer* Foreground = _tileMap->getLayer("Foreground_layer");
-	if (getTileProperty(TileCoord) == TilePropertyCollectable)
-		_metaLayer->removeTileAt(TileCoord);//verhindert, dass Collision Tiles entfernt werden k�nnen;
-	Foreground->removeTileAt(TileCoord);
+	if (getTileProperty(TileCoord) == TilePropertyCollectable) {
+		_metaLayer->removeTileAt(TileCoord); //verhindert, dass Collision Tiles entfernt werden k�nnen;
+		TMXLayer* Foreground = _tileMap->getLayer("Foreground_layer");
+		Foreground->removeTileAt(TileCoord);
+	}
 }
