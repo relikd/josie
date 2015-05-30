21387d690fe294718f84ae5a03024f078147c793
relikd <info@relikd.de>
2015-05-30 19:57:38 +0200

    more shoppy

--- JosieGame/Classes/ShopScene.h
+++ JosieGame/Classes/ShopScene.h
@@ -7,12 +7,20 @@ class ShopScene : public cocos2d::Layer {
 public:
 	ShopScene();
 	virtual ~ShopScene();
-	virtual bool init();
+	void createButtons();
 
 	static cocos2d::Scene* createScene();
-	CREATE_FUNC(ShopScene);
 
-	void upgrade(Ref* p);
+	void perkDisable(cocos2d::MenuItemImage *btn);
+	void perkEnable(cocos2d::MenuItemImage *btn);
+	void perkEquip(cocos2d::MenuItemImage *btn);
+
+	void upgradeShoot(Ref* p);
+	void upgradeDamage(Ref* p);
+	void upgradeFrequency(Ref* p);
+	void upgradeSpeed(Ref* p);
+	void upgradeShield(Ref* p);
+	void upgradeHeart(Ref* p);
 };
 
 #endif /* SHOPSCENE_H_ */
