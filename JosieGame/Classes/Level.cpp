68a7f434a10fc02d0ea6e59d45bbd73f417a93f5
relikd <info@relikd.de>
2015-06-16 18:04:17 +0200

    Level nach kurzer Pause starten

--- JosieGame/Classes/Level.cpp
+++ JosieGame/Classes/Level.cpp
@@ -25,6 +25,7 @@ Level* Level::initWithLevel(int level, int sublevel) {
 
 	AudioUnit::playLevelBackground();
 	l->createUI(level, sublevel);
+	l->startAfterDelay(2.0);
 
 	return l;
 }
@@ -61,6 +62,24 @@ void Level::createUI(int lvl, int sublvl)
 	this->addChild(mapManager);
 	mapManager->addChild(player);
 	this->addChild(hud);
+}
+
+void Level::startAfterDelay(float delay)
+{
+	hud->unscheduleUpdate(); // prevent player running
+
+	Label *countdown = Label::createWithTTF("3", "fonts/Marker Felt.ttf", 80);
+	countdown->setPosition(Director::getInstance()->getWinSize()/2);
+	this->addChild(countdown);
+
+	DelayTime *wait = DelayTime::create(delay/3.0);
+	CallFuncN *set2 = CallFuncN::create(CC_CALLBACK_0(Label::setString, countdown, "2"));
+	CallFuncN *set1 = CallFuncN::create(CC_CALLBACK_0(Label::setString, countdown, "1"));
+	CallFuncN *rmLabel = CallFuncN::create(CC_CALLBACK_0(Node::removeFromParent, countdown));
+	CallFuncN *startHUD = CallFuncN::create(CC_CALLBACK_0(Node::scheduleUpdate, hud));
+
+	Sequence *seq = Sequence::create(wait,set2,wait,set1,wait,rmLabel,startHUD,nullptr);
+	this->runAction(seq);
 }
 
 
