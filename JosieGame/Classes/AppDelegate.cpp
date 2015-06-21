ba1c3822bbd349cbf30bfb9a14abacc5fe26e6d7
Daniel Glück <hello@danielglueck.de>
2015-06-21 17:11:54 +0200

    Gammeltet Manager nur 1 mal initialisieren

--- JosieGame/Classes/AppDelegate.cpp
+++ JosieGame/Classes/AppDelegate.cpp
@@ -37,8 +37,9 @@ bool AppDelegate::applicationDidFinishLaunching() {
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0 / 60);
 
-
+    if (UserDefaults::getInstance().getBoolForKey("josie_save_state", false)==false){
     GameStateManager::initSpriteCache();
+    }
 
     director->runWithScene(MainMenu::createScene());
 
