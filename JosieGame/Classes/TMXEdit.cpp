9e52336456b7b595b8517eb16c6e266b609f36a6
Jonas Kaiser <hello@kaiserjonas.de>
2015-06-23 08:25:05 +0200

    levelende passt nun auch im worst-case

--- JosieGame/Classes/TMXEdit.cpp
+++ JosieGame/Classes/TMXEdit.cpp
@@ -61,7 +61,7 @@ void TMXEdit::fillLevel() {
 	int width = map->getMapSize().width;
 	int x = 20;
 	int height = 13;
-	while (x < width - (38 + 2 * PARTLENGTH)) {
+	while (x < width - (46 + 2 * PARTLENGTH)) {
 
 		switch (arc4random() % 10) {
 		case 0:
